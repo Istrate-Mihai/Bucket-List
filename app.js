a5c5ef2b15d021ae36a201c6fc35b68deca1a8dd
Istrate Mihai <i.mihai9960@gmail.com>
2021-03-30 19:36:55 +0300

    Added functionality to the js file
    It contains a function validateForm() that verifies if the form was completed and prints the input to the screen

--- app.js
+++ app.js
@@ -1,18 +1,53 @@
-document.addEventListener('DOMContentLoaded',()=>{
 
-    var form=document.forms[0];
-    var formButton=form['submitButton'];
-    
-    formButton.addEventListener("click",call);
-    
-    
-    function call(){
-    
-        document.write("The form was submitted,to be continued!");
-    }
+var errorParagraph=document.getElementById("errorMessage");
+
+function validateForm(form){
+  
+     if(form.Activity_Title.value != "" && form.Activity_Content.value != "" ){
+        errorParagraph.innerHTML="Your last sent activity to the bucket list was <br> "+"Title: "+ form.Activity_Title.value +"<br>"+"Description: "+form.Activity_Content.value;
+       return true;
+     }else{
+        errorParagraph.innerHTML="You must complete the entire form!";
+        return false;
+     }
+     
+
+}
 
 
 
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
 
 
