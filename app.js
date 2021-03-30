e1c42649135625ee9202f95b3bc894b5d15c4a7f
Istrate Mihai <i.mihai9960@gmail.com>
2021-03-30 19:40:58 +0300

    Reduced a bunch of empty lines

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var errorParagraph=document.getElementById("errorMessage");
 function validateForm(form){
   
      if(form.Activity_Title.value != "" && form.Activity_Content.value != "" ){
-        errorParagraph.innerHTML="Your last sent activity to the bucket list was <br> "+"Title: "+ form.Activity_Title.value +"<br>"+"Description: "+form.Activity_Content.value;
+        errorParagraph.innerHTML="Your last sent activity to vathe bucket list was <br> "+"Title: "+ form.Activity_Title.value +"<br>"+"Description: "+form.Activity_Content.value;
        return true;
      }else{
         errorParagraph.innerHTML="You must complete the entire form!";
@@ -13,41 +13,5 @@ function validateForm(form){
      
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
 
 
