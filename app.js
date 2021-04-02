a13df3e0b004a930d69f85eaff2b47502fa39847
Istrate Mihai <i.mihai9960@gmail.com>
2021-04-02 14:57:56 +0300

    Created a function to display the Bucket List
    The function is called ShowList and transforms the localStorage Object into an array than diplays it!

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ function validateForm(form){
   
      if(form.Activity_Title.value != "" && form.Activity_Content.value != "" ){
         errorParagraph.innerHTML="Your last sent activity to vathe bucket list was <br> "+"Title: "+ form.Activity_Title.value +"<br>"+"Description: "+form.Activity_Content.value;
+        localStorage.setItem(form.Activity_Title.value,form.Activity_Content.value);
+       
        return true;
      }else{
         errorParagraph.innerHTML="You must complete the entire form!";
@@ -15,3 +17,18 @@ function validateForm(form){
 }
 
 
+function ShowList(){
+
+var x,txt="",localStorageArray;
+
+var localStorageArray=Object.values(localStorage);
+
+for( x in localStorageArray ){
+   
+   txt+=localStorageArray[x]+"<br>";
+
+}
+
+errorParagraph.innerHTML=txt;
+
+}
